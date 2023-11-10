e67054c24243e93cf6aaf64602a776a8295fcdc6
victoresser <victorribeiroesser@gmail.com>
2023-11-10 18:09:34 -0300

    Primeiro commit. ScreenSound Web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarrosselComponent } from './components/carrossel/carrossel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { IgxCarouselModule, IgxSliderModule } from 'igniteui-angular';
+import { HeaderLogoComponent } from './components/header-logo/header-logo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DownloadsComponent } from './components/downloads/downloads.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarrosselComponent,
+    HeaderLogoComponent,
+    FooterComponent,
+    DownloadsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    IgxCarouselModule,
+    IgxSliderModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/header-logo/header-logo.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { HeaderLogoComponent } from './header-logo.component';
+
+describe('HeaderLogoComponent', () => {
+  let component: HeaderLogoComponent;
+  let fixture: ComponentFixture<HeaderLogoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HeaderLogoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderLogoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
