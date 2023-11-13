df60d12158145a04c003e2c2ba1486456645fede
victoresser <victorribeiroesser@gmail.com>
2023-11-13 17:37:09 -0300

    Ajustes de Rotas do Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarrosselComponent } from './components/carrossel/carrossel.component';
+import { CarrosselComponent } from './components/home/carrossel/carrossel.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { IgxCarouselModule, IgxSliderModule } from 'igniteui-angular';
 import { HeaderLogoComponent } from './components/header-logo/header-logo.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { DownloadsComponent } from './components/downloads/downloads.component';
+import { HomeComponent } from './components/home/home.component';
+import { DownloadsComponent } from './components/home/downloads/downloads.component';
+import { NovidadesComponent } from './components/home/novidades/novidades.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     CarrosselComponent,
     HeaderLogoComponent,
     FooterComponent,
-    DownloadsComponent
+    DownloadsComponent,
+    NovidadesComponent
   ],
   imports: [
     BrowserModule,
@@ -29,5 +33,6 @@ import { DownloadsComponent } from './components/downloads/downloads.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
