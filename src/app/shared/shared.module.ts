ae90fc14864a1c802ce7d595ed927fc545f6437f
victoresser <victorribeiroesser@gmail.com>
2024-03-25 17:39:09 -0300

    Ajustes em Module e Templates HTML.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { InputFormFieldComponent } from './input-form-field/input-form-field.component';
 import { HeaderLogoComponent } from './header-logo/header-logo.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
 	declarations: [InputFormFieldComponent, HeaderLogoComponent, FooterComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, AppRoutingModule, HttpClientModule],
 	exports: [InputFormFieldComponent, HeaderLogoComponent, FooterComponent],
 })
 export class SharedModule {}
